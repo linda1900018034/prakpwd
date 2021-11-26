ab18fac5788ad7e971ffd385769fef818d7708c6
linda1900018034 <92171255+linda1900018034@users.noreply.github.com>
2021-11-26 19:25:58 +0700

    Add files via upload

--- input_user.php
+++ input_user.php
@@ -1,7 +1,7 @@
 <?php
 include "koneksi.php";
 $id_user = $_POST['id_user'];
-$nama = $_POST['nama'];
+$nama = $_POST['nama_lengkap'];
 $email = $_POST['email'];
 $pass = md5($_POST[password]);
 $sql = "INSERT INTO users(id_user,password, nama, email) VALUES ('$id_user', '$pass',
