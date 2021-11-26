9dfcfd9f0ec29fa55abfab88a8aa1c850dc137c6
linda1900018034 <92171255+linda1900018034@users.noreply.github.com>
2021-11-26 19:26:28 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
-$host="localhost";
-$username="root";
-$password="";
-$databasename="akademikkk";
-$con=@mysqli_connect($host,$username,$password,$databasename);
+$host="localhost"; // untuk menhbungkan pada localhost atau browser atau menuju ke website
+$username="root"; // yg akan terhubung ke database dilakukan root melalui xampp
+$password=""; 
+$databasename="akademikkk"; // untuk menyambungkan nama database yg akan dihubungkan ke dlam phpmyadmin
+$con=@mysqli_connect($host,$username,$password,$databasename); // jika database tdk terbaca bagian ini akan melakukan conecting ke database
 if (!$con) {
  echo "Error: " . mysqli_connect_error();
 exit();
